d03f86c0257bcf532993fe768c69144ecec97687
Ian <ianperrin@gmail.com>
2021-07-15 13:17:15 +0100

    Refactor layouts and includes

--- assets/scripts/bwr-main.js
+++ assets/scripts/bwr-main.js
@@ -1,14 +1,3 @@
-// https://stackoverflow.com/questions/42401606/how-to-hide-collapsible-bootstrap-navbar-on-click
-const menuToggle = document.getElementById('navbarNavBwr');
-const navLinks = menuToggle.querySelectorAll('li.nav-item:not(.dropdown)');
-if (menuToggle) {
-  const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
-  navLinks.forEach((l) => {
-    l.addEventListener('click', () => { 
-      if (menuToggle.classList.contains('show')) { bsCollapse.toggle(); } 
-    });
-  })
-}
 // Local variables
 const mapMarkers = {
   drink: getMarker('fa-coffee', 'red', 'circle', 'fas'),
---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+// https://stackoverflow.com/questions/42401606/how-to-hide-collapsible-bootstrap-navbar-on-click
+const menuToggle = document.getElementById('navbarNavCollection');
+if (menuToggle) {
+  const navLinks = menuToggle.querySelectorAll('li.nav-item:not(.dropdown)');
+  const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+  navLinks.forEach((l) => {
+    l.addEventListener('click', () => { 
+      if (menuToggle.classList.contains('show')) { bsCollapse.toggle(); } 
+    });
+  })
+}
